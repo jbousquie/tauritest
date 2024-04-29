9290757c04e85781ea53aa920bad03b3e2ac5b74
Jérôme Bousquié <jerome.bousquie@iut-rodez.fr>
2024-04-29 17:40:30 +0200

    test affichage

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { invoke } from "@tauri-apps/api/core";
 interface Results {
     ldap_attrs: [string];
     ad_attrs: [string];
-    ldap_res: [{}];
-    ad_res: [{}];
+    ldap_res: [];
+    ad_res: [];
 }
 
 let inputEl: HTMLInputElement | null;
@@ -13,7 +13,7 @@ let inputEl: HTMLInputElement | null;
 async function search() {
     if (inputEl) {
       let results: Results = await invoke("search_results", { filter: inputEl.value, });
-      console.log(results);
+      //console.log(results);
       displayResults(results);
     }
 }
@@ -22,16 +22,35 @@ async function search() {
 function displayResults(results: Results) {
     let resElem = document.querySelector('#resultats');
     if (resElem) {
-        let resultsHtml = displayList(results.ldap_res) + displayList(results.ad_res);
+        let resultsHtml = displayList(results.ldap_attrs, results.ldap_res) + displayList(results.ad_attrs, results.ad_res);
         resElem.innerHTML = resultsHtml;      
     }
 }
 
-function displayList(data: [{}]): string {
+function displayList(attrs: [string], data: []): string {
     let listHtml = '<div>';
     // header
-
-
+    listHtml = listHtml + '<div class="header">';
+    for (let h = 0; h < attrs.length; h++) {
+        listHtml = listHtml + '<span>' + attrs[h] + '</span>';
+    }
+    listHtml = listHtml + '</div><br/><div>';
+    // data
+    for (let i = 0;  i < data.length; i++) {
+        let line = data[i];
+        attrs.forEach(attr => {
+            let vct: [string] = line[attr];
+            let val: string = '';
+            if (vct && vct.length > 0) {
+                val = vct[0];
+                for (let i = 1; i < vct.length; i++) {
+                    val = "\n" + vct[i];
+                }
+            }
+            listHtml = listHtml + '<span>' + val + '</span>';
+        });
+        listHtml = listHtml + '</div><br/>';
+    }
     listHtml = listHtml + '</div>';
     return listHtml;
 }
